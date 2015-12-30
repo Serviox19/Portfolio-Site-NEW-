8054d350a3fb1f2abf49e9d7c64b7f1ff2b5d489
Serviox19 <servio19@gmail.com>
2015-12-29 19:25:02 -0500

    made ajax work to display repos

--- js/projects.js
+++ js/projects.js
@@ -1,19 +1,28 @@
 $(document).ready(function () {
-  $("#github-click").click(function (e) {
-    e.preventDefault();
 
-    $.ajax({
-    type: "GET",
-    url: "https://api.github.com/users/serviox19/repos",
-    success: function(repos){
-      for (var i = 0; i < repos.length; i++) {
-
-      }
+  $.ajax({
+  type: "GET",
+  url: "https://api.github.com/users/serviox19/repos",
+  success: function(repos) {
+    for (var i = 0; i < repos.length; i++) {
+    var newListItem = buildListGroup(repos[i]);
+      $(".list-group").append(newListItem);
+    }
     },
     error: function(jqXHR, textStatus, errorThrown) {
       alert("Something went Wrong!")
     }
     });
 
-  });
+  function buildListGroup(repoData) {
+    var apiUrl = "https://api.github.com/repos/";
+    apiUrl += repoData.owner.login + "/";
+    apiUrl += repoData.name;
+
+    var newLink = $("<a>")
+      .attr("href", apiUrl)
+      .append(repoData.full_name)
+      .addClass("list-group-item")
+    return newLink;
+  };
 });
