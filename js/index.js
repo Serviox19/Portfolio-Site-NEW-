0cdcd01ad284c6464cd0b961878a29c44c0cff45
Serviox19 <servio19@gmail.com>
2016-02-08 19:15:53 -0500

    minor refactoring

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+// Gets all my repos
   $.ajax({
   type: "GET",
   url: "https://api.github.com/users/serviox19/repos",
@@ -25,4 +26,18 @@ $(document).ready(function () {
       .addClass("list-group-item")
     return newLink;
   };
+
+
+// When repo is clicked, all info will appear
+  $(".list-group").on("click", "a", function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: "GET",
+      url: $(this).attr("href"),
+      success: function (repoData) {
+        console.log(repoData);
+      }
+    })
+  });
 });
